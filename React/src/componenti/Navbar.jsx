84376ec8c21263be1abd5a44474cea44cc233403
DestMarco <marcolino1424@gmail.com>
2024-11-28 19:24:49 +0100

    ++

--- React/src/componenti/Navbar.jsx
+++ React/src/componenti/Navbar.jsx
@@ -1,14 +1,17 @@
 import './Navbar.css'
 import Link from './Link'
-const X =3;
+const X =11;
 const img="vite";
+/*const imgstyle={height:"300px", bordeRadus:"90px"}*/
+/*const imgstyle={height:X<10?"800px":"500px", borderRadius:"500px"}*/
 
 function Navbar(){
 return(
 
     <ul>
+    <div id ="box" className='rotate'>Lodate oil sole </div>
     <nav>{X}</nav>
-    <img src={`/${img}.svg`}></img>
+    <img className="BordoArrotondato"src={`/${img}.svg`}></img>
     <li><Link></Link></li>
     <li><Link></Link></li>
     <li><Link></Link></li>
