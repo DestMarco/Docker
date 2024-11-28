d8fbcc80a991a1bc3a37f3e78d7ec28303cfc33f
DestMarco <marcolino1424@gmail.com>
2024-11-28 17:43:54 +0100

    ++

--- React/src/componenti/Navbar.jsx
+++ React/src/componenti/Navbar.jsx
@@ -1,8 +1,14 @@
 import './Navbar.css'
 import Link from './Link'
+const X =3;
+const img="vite";
+
 function Navbar(){
 return(
+
     <ul>
+    <nav>{X}</nav>
+    <img src={`/${img}.svg`}></img>
     <li><Link></Link></li>
     <li><Link></Link></li>
     <li><Link></Link></li>
