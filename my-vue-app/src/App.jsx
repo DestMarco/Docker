32c6804b11a1a7289e6ac0f09d95f31c5144b023
DestMarco <marcolino1424@gmail.com>
2024-12-05 13:58:24 +0100

    ++

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Card from './componenti/Card'
 import './App.css'
 
 function App() {
@@ -8,6 +9,31 @@ function App() {
 
   return (
     <>
+    <Card 
+    isVisit={false}
+    ti="Loda il sole" 
+    des="soleir fa lodare il sole alle persone"
+    im="https://i.makeagif.com/media/5-19-2017/47ZHUC.gif">
+    </Card>
+    <Card
+    isVisit={true}
+    ti="Siegmeyer" 
+    des=" Aknight swearing allegiance to an unidentified order of warriors, this NPC can occasionally assist the player in various points in the game."
+    im="https://darksouls.wiki.fextralife.com/file/Dark-Souls/siegmeyer_of_catarina.jpg"
+    ></Card>
+    <Card
+    isVisit={false}
+    ti="lautrec di carim" 
+    des="Lautrec is found imprisoned on the top floor of the church in Undead Parish, before climbing the ladders to the roof where the Bell Gargoyles are fought."
+    im="https://i.pinimg.com/736x/92/4e/cb/924ecb058a7db8af8e1deb9a09ff18f5.jpg">
+    </Card>
+    <Card
+    isVisit={true}
+    ti="Smough" 
+    des="Smough is the royal executioner of Anor Londo. He longs to be ranked with the Four Knights, but his cruelty, including using his victim's bone as his food's "
+    im="https://i.pinimg.com/736x/0a/ba/2a/0aba2a29134162f9de4d605e8e44429f.jpg">
+    
+    </Card>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ my-vue-app/src/componenti/Card.jsx
@@ -0,0 +1,27 @@
+//import"./card.css"
+
+function Card({ti,im,des,isVisit}){
+
+    /*function Card(props)
+     const ti = ti.props*/
+
+    //const ti=ti
+    //const im=im
+    //const des=des
+    return(
+    <div> 
+        <img src={im} alt=""></img>
+        <div>
+
+        </div>
+        <h2> {ti}</h2>
+        <p> {des}</p>
+
+        {isVisit ?  <span><b> Ucciso</b></span>:<span>Non ucciso </span> }
+    </div>
+    )
+}
+
+
+
+export default Card;
